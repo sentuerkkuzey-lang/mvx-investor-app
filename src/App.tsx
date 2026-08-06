56699cce9677abb405ca872b1ca9adf687c24fd6
sentuerkkuzey-lang <sentuerk.kuzey@gmail.com>
2026-08-06 06:45:31 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Login from "./pages/Login";
 import ForcePasswordChange from "./pages/ForcePasswordChange";
 import Dashboard from "./pages/Dashboard";
 import OwnerPanel from "./pages/OwnerPanel";
+import Polls from "./pages/Polls";
+import Profile from "./pages/Profile";
+import News from "./pages/News";
+import Documents from "./pages/Documents";
 import { RequireAuth, RequireOwner } from "./components/RouteGuards";
 
 export default function App() {
@@ -27,6 +31,42 @@ export default function App() {
             <RequireOwner>
               <OwnerPanel />
             </RequireOwner>
+          </RequireAuth>
+        }
+      />
+
+      <Route
+        path="/abstimmungen"
+        element={
+          <RequireAuth>
+            <Polls />
+          </RequireAuth>
+        }
+      />
+
+      <Route
+        path="/news"
+        element={
+          <RequireAuth>
+            <News />
+          </RequireAuth>
+        }
+      />
+
+      <Route
+        path="/dokumente"
+        element={
+          <RequireAuth>
+            <Documents />
+          </RequireAuth>
+        }
+      />
+
+      <Route
+        path="/profil"
+        element={
+          <RequireAuth>
+            <Profile />
           </RequireAuth>
         }
       />
